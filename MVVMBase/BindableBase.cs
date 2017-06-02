ab54d1e6a1518c7d18af4049dcf8db3430264778
unknown <fujin13666@gmail.com>
2017-06-03 00:30:45 +0300

    Bindable base

--- MVVMBase/BindableBase.cs
+++ MVVMBase/BindableBase.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel;
+using MVVMBase.Annotations;
 
 namespace MVVMBase
 {
-    public class Class1
+    public class BindableBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool SetProperty<T>(ref T target, T value, string propertyName = null)
+        {
+            if (target.Equals(value)) return false;
+
+            target = value;
+
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
